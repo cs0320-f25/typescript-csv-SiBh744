439b74341921427b7e62bc61c53519cbfeb82880
SiBh744 <siddharth_bhagwat@brown.edu>
2025-09-11 16:34:49 -0400

    Adjusted he parser to take in a Zod schema

--- tests/basic-parser.test.ts
+++ tests/basic-parser.test.ts
@@ -1,5 +1,6 @@
 import { parseCSV } from "../src/basic-parser";
 import * as path from "path";
+import z from 'zod';
 
 const PEOPLE_CSV_PATH = path.join(__dirname, "../data/people.csv");
 
@@ -22,7 +23,7 @@ test("parseCSV yields only arrays", async () => {
 });
 
 test("parseCSV handles commas withing quotes", async () => {
-  const results = await parseCSV(path.join(__dirname, "../data/commas.csv")) /* what to do here? */
+  const results = await parseCSV(path.join(__dirname, "../data/commas.csv"))
   expect(results).toHaveLength(3);
   expect(results[0]).toEqual(["name", "age", "comment"]);
   expect(results[1]).toEqual(["Yeeticus", "23", "Hello, world!"]);
@@ -42,6 +43,14 @@ test("parseCSV empty fields", async () => {
   expect(results[2]).toEqual(["", "30", ""]);
 });
 
+test("parseCSV double quotes within quotes", async () => {
+  const results =
+    await parseCSV(path.join(__dirname, "../data/double-quotes.csv"))
+  expect(results).toHaveLength(2);
+  expect(results[0]).toEqual(['"', '3', '7']);
+  expect(results[1]).toEqual(['he "said"', '5', '']);
+});
+
 test("parseCSV line breaks within quotes", async () => {
   const results = await parseCSV(path.join(__dirname, "../data/line-break.csv"))
   expect(results).toHaveLength(3);
@@ -49,6 +58,19 @@ test("parseCSV line breaks within quotes", async () => {
   expect(results[1]).toEqual(["Yeeticus", "22", "Hello\nworld!"]);
   expect(results[2]).toEqual(["Bob", "3", "1\n2\n3\n4"]);
 });
+
+test("parseCSV with schema", async () => {
+  const schema = z.array(z.string().min(1));
+  const results = await parseCSV(PEOPLE_CSV_PATH, schema)
+  expect(results).toHaveLength(5);
+  expect(results[0]).toEqual(["name", "age"]);
+  expect(results[1]).toEqual(["Alice", "23"]);
+  expect(results[2]).toEqual(["Bob", "thirty"]);
+  expect(results[3]).toEqual(["Charlie", "25"]);
+  expect(results[4]).toEqual(["Nim", "22"]);
+});
+
+
 
 
 
